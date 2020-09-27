7ea5e9a4f35bd8056a9cc872f468ffeee2c8182c
hc_zhang <hc_zhang@trip.com>
2020-09-27 20:02:05 +0800

    update

--- 0-Build-Publish-Move.bat
+++ 0-Build-Publish-Move.bat
@@ -2,13 +2,12 @@ dotnet restore
 
 msbuild Savory.Canos.Engine.ElementUI.sln /p:configuration=release
 
-nuget push Savory.Canos.Engine.ElementUI\bin\Release\Savory.Canos.Engine.ElementUI.*.nupkg -source https://package.savory.cn/repository/savory-nuget-framework/
+nuget push Savory.Canos.Engine.ElementUI\bin\Release\Savory.Canos.Engine.ElementUI.*.nupkg -source https://package.savory.cn/v3/index.json
+nuget push Savory.Canos.Engine.ElementUI.Resources\bin\Release\Savory.Canos.Engine.ElementUI.Resources.*.nupkg -source https://package.savory.cn/v3/index.json
+nuget push Savory.Canos.Manager.ElementUI\bin\Release\Savory.Canos.Manager.ElementUI.*.nupkg -source https://package.savory.cn/v3/index.json
+
 move /Y Savory.Canos.Engine.ElementUI\bin\Release\Savory.Canos.Engine.ElementUI.*.nupkg D:\LocalSavoryNuget\
-
-nuget push Savory.Canos.Engine.ElementUI.Resources\bin\Release\Savory.Canos.Engine.ElementUI.Resources.*.nupkg -source https://package.savory.cn/repository/savory-nuget-framework/
 move /Y Savory.Canos.Engine.ElementUI.Resources\bin\Release\Savory.Canos.Engine.ElementUI.Resources.*.nupkg D:\LocalSavoryNuget\
-
-nuget push Savory.Canos.Manager.ElementUI\bin\Release\Savory.Canos.Manager.ElementUI.*.nupkg -source https://package.savory.cn/repository/savory-nuget-framework/
 move /Y Savory.Canos.Manager.ElementUI\bin\Release\Savory.Canos.Manager.ElementUI.*.nupkg D:\LocalSavoryNuget\
 
 pause
